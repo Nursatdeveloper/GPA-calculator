e0c5c81d2bbc605a854ef94a8f470b19d0472408
Nursat Zeinolla <kznursat@gmail.com>
2022-02-03 15:27:44 +0600

    first commit

--- nu-gpa-calculator/Startup.cs
+++ nu-gpa-calculator/Startup.cs
@@ -27,7 +27,7 @@ namespace nu_gpa_calculator
         }
 
         // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
-        public void Configure(IApplicationBuilder app, IWebHostEnvironment env)
+        public void Configure(IApplicationBuilder app)
         {
             app.UseDeveloperExceptionPage();
             app.UseHttpsRedirection();
